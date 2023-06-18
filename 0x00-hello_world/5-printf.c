0e3495bc99e118992c4d3c56f263de75425993fd
cloudezez <cloudezez@gmail.com>
2023-06-17 21:35:08 -0700

    6-size.c6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,13 +4,13 @@
 /**
  * Main - prints "with proper grammer, but the outcome is a piece of art,"
  * followed by newline
- * Return: 0 (success)
+ * Return: Always 0 (success)
  */
 
 
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
