0930f86674b6c3fe15010890dd0fea8081e27158
cloudezez <cloudezez@gmail.com>
2023-06-21 03:16:14 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,19 +11,14 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2;
 
-	while (i < 50)
+	printf("%ld, %ld", j, k);
+
+	for (i = 2; i < 50; ++i);
 	{
-	if (i == 0)
-	printf("%ld", j);
-	else if (i == 1)
-	printf(", %ld", k);
-	else
-	{
-	k += j;
-	j + k - j;
-	printf(", %ld", k);
-	}
-	++i;
+		long temp == k;
+		k += j;
+		j = temp;
+		printf(", %ld", k);
 	}
 	printf("\n");
 	return (0);
