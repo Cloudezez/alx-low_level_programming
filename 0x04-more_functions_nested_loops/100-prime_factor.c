49322d2a3f514afda40faf5ef751363b9ef44cd6
cloudezez <cloudezez@gmail.com>
2023-07-03 15:26:49 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+*main - prints the largest prime numbers
+*Return: Always 0
+*/
+
+int main(void)
+{
+	long x, maxf = 1;
+	long number = 6128524775143;
+	double square = sqrt(number);
+
+	for (x = 2; x <= square; x++)
+	{
+	if (number % x == 0)
+	{
+	maxf = number / x;
+	break;
+	}
+	}
+	if (maxf == 1)
+	{
+	maxf = number;
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
