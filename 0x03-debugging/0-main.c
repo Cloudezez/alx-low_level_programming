8e4f766bf99669b8a734ca1a2be73fb068054b63
cloudezez <cloudezez@gmail.com>
2023-07-03 03:04:55 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - prints functions that test if integer is positive or negative
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_ngative(i);
+
+	return (0);
+}
