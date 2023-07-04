f1c8531855dc4dcc5a30637f8d99cc4869022d4c
cloudezez <cloudezez@gmail.com>
2023-07-04 11:33:38 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
+/**
+*_strstr - entry point
+*@haystack: input
+*needle: input
+*Return: Always 0
+*/
+
 char *_strstr(char *haystack != '\0'; haystack++)
 {
-	for(; *haystack !='\0'; haystack++)
+	for(; *haystack != '\0'; haystack++)
 	{
-		char *l = haystack
-		char *p = needle;
+	char *l = haystack
+	char *p = needle;
 
-		while (*l ==*p && *p != '\0')
-		{
-			l++;
-			p++;
-		}
-		if(*p =='\0')
-			return (haystack);
+	while (*l == *p && *p != '\0')
+	{
+	l++;
+	p++;
+	}
+	if(*p == '\0')
+	return (haystack);
 	}
 	return (0);
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+*print_chessboard - Entry point
+*@a: array
+*Return: Always 0
+*/
+
 void print_chessboard(char (*a)[8])
 {
 	int i;
@@ -7,8 +13,8 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i <8; i++)
 	{
-		for (j = 0; j <8; j++)
-			_putchar(a[i][j]);
-		-putchar('\n')
+	for (j = 0; j <8; j++)
+	_putchar(a[i][j]);
+	_putchar('\n');
 	}
 }
