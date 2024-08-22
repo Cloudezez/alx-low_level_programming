166f79d5ad8c4620489097e1151454ee1fc680ca
cloudezez <cloudezez@gmail.com>
2024-08-21 20:30:00 -0700

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,37 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
-*print_numbers - prints an integer.
-*@n: The integer to print.
-*Return: Nothing
-*/
+ * print_number - function that prints an integer..
+ * @n: input value to check
+ * Return: nothing.
+ */
 
-void print_numbers(int n)
+void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
 
 	if (n < 0)
 	{
-	n *= -1;
-	k = n;
-	_putchar('_');
+		n = n * -1;
+		_putchar('-');
 	}
-	k /= 10;
-	if (k != 0)
-	print_numbers(k);
-	_putchar((unsigned int) n % 10 + '0');
+	num = n;
+	num2 = num;
+	if (num > 9)
+	{
+		while (num >= 10)
+		{
+			aux = aux * 10;
+			num = num / 10;
+		}
+		_putchar((num2 / aux) + '0');
+		aux = aux / 10;
+
+		for (i = aux; i >= 1; i = i / 10)
+			_putchar((num2 / i) % 10 + '0');
+	}
+	else
+		_putchar(num + '0');
 }
