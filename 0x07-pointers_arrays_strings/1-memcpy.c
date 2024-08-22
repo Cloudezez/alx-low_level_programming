9fb2c961e63972c2a4aa1a5b294440193e381678
cloudezez <cloudezez@gmail.com>
2024-08-21 17:50:15 -0700

    m

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-*_mempy - copy stored data
-*@dest: copied data
-*src: copied integer
-*Return: dest
-*/
+ * *_memcpy - main function.
+ * @dest: Pointer to destination.
+ * @src: Pointer to the source.
+ * @n: Number of bytes to copy.
+ *
+ * Description: This function copies memory area.
+ *
+ * Return: A pointer to dest.
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r = 0;
-	int i = n;
+	unsigned int i = 0;
 
-	for(; r < i; r++)
+	while (i < n)
 	{
-	dest[r]=src[r];
-	n--;
+		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
